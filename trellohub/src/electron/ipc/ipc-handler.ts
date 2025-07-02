09d2f0b2249b1f4388d8058d835e5e45884d6557
enerzilius <efblouback@gmail.com>
2025-07-02 20:25:36 -0300

    abandono dessa branch

--- trellohub/src/electron/ipc/ipc-handler.ts
+++ trellohub/src/electron/ipc/ipc-handler.ts
@@ -1,13 +1,11 @@
 import { ipcMain } from 'electron';
 import { githubHandlers } from './handlers/github-handlers.js';
-import { redisHandlers } from './handlers/redis-handler.js';
 import { systemHandlers } from './handlers/system-handlers.js';
 
 export function registerIpcHandlers(): void {
     console.log('Registrando handlers IPC...');
 
     githubHandlers.register();
-    redisHandlers.register();
     systemHandlers.register();
 
     console.log('Handlers IPC registrados com sucesso');
@@ -17,7 +15,6 @@ export function unregisterIpcHandlers(): void {
     console.log('Removendo handlers IPC...');
 
     githubHandlers.unregister();
-    redisHandlers.unregister();
     systemHandlers.unregister();
 
     console.log('Handlers IPC removidos');
