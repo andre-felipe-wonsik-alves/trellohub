c3dc59ecee182c0f63082a345c4d6856d49fa5d1
raquellhwa <raquelhelenawonsik@gmail.com>
2025-07-02 21:17:25 -0300

    update na estrutra do front e criando o esqueleto do observer

---
+++ trellohub/src/electron/utils/http/http-observer.ts
@@ -0,0 +1,20 @@
+type Subscriber = (error: any) => void;
+
+// https://refactoring.guru/design-patterns/observer
+class Observer {
+    private subscribers: Subscriber[]=[];
+
+    subscribe(subscriber: Subscriber){
+        this.subscribers.push(subscriber);
+    }
+
+    unsubscribe(subscriber: Subscriber){
+        this.subscribers = this.subscribers.filter(elem => elem !== subscriber);
+    }
+
+    notify(error: any){
+        this.subscribers.forEach(subscriber => subscriber(error));
+    }
+}
+
+export const observer = new Observer();
