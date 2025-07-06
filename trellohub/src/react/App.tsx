fbf7c8224b35446ff9e398b347d2c6dc5795b5da
Gustosa <gustavomartins5820@gmail.com>
2025-07-05 21:04:01 -0300

    Create user repositories list component

--- trellohub/src/react/App.tsx
+++ trellohub/src/react/App.tsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Board from "./components/Board";
+import RepositoriesList from "./components/RepositoriesList";
 
 const App: React.FC = () => {
-  const [connectStatus, setConnectStatus] = useState('Disconnected');
-  
-  const [result] = useState(0);
+  const [repositories, setRepositories] = useState<any[]>([]);
+  const [selectedRepository, setSelectedRepository] = useState<any | null>(null);
+  const [auth, setAuth] = useState<boolean>(false);
+
   const handleLogin = async () => {
+    console.log(1)
     const token = await window.electronAPI.make_login();
-    console.log(token);
+    console.log(token)
+    // const repos = await window.electronAPI.getUserRepositories(token.token);
+    // setRepositories(repos);
+    // setAuth(true);
+  };
+
+  const handleRepositoryClick = (repo: any) => {
+    setSelectedRepository(repo);
   };
 
   useEffect(() => {
@@ -16,7 +26,17 @@ const App: React.FC = () => {
   
   return (
     <div className="min-h-screen">
-      <Board />
+      {!auth ? (
+        <div></div>
+      ) : (
+        <div>
+          {!selectedRepository && (
+            <RepositoriesList user_repositories={repositories} onRepositoryClick={handleRepositoryClick} />
+          )}
+
+          {selectedRepository && <Board />}
+        </div>
+      )}
     </div>
   );
 };
---
+++ trellohub/src/react/components/RepositoriesList.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const RepositoriesList: React.FC<any> = ({ user_repositories, onRepositoryClick }) => {
+    return (
+        <div>
+            <h1> Seus repositórios </h1>
+            <ul>
+                {user_repositories.map((repo: any) => {
+                    <li>
+                        <button onClick={() => onRepositoryClick(repo)}>
+                            {repo.name}
+                        </button>
+                    </li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default RepositoriesList;
