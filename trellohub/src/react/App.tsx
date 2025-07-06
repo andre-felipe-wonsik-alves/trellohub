9dac90fe86cb1296992950495000365f8cd373fa
andre-felipe-wonsik-alves <aalves.2020@alunos.utfpr.edu.br>
2025-07-06 20:46:37 -0300

    tirando um log que esqueci

--- trellohub/src/react/App.tsx
+++ trellohub/src/react/App.tsx
@@ -6,21 +6,21 @@ import Button from "./components/ui/button";
 const App: React.FC = () => {
   const [github_user_info, set_github_user_info] = useState<any>({});
   const [repositories, set_repositories] = useState<any[]>([]);
-  const [selected_repository, set_selected_repository] = useState<any | null>(null);
+  const [selected_repository, set_selected_repository] = useState<any | null>(
+    null
+  );
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = async () => {
     const result = await window.electronAPI.make_login();
     if (result.success) {
       window.electronAPI.saveToken(result.token);
-      const token = await window.electronAPI.getToken();
-      console.log(token);
       setIsLoggedIn(true);
       const token = result.token.access_token;
       const user = await window.electronAPI.getAuthenticatedUser(token);
       set_github_user_info({
         token,
-        user
+        user,
       });
 
       const repos = await window.electronAPI.getUserRepositories(token);
@@ -30,7 +30,6 @@ const App: React.FC = () => {
     }
   };
 
-
   const handleRepositoryClick = async (repo: any) => {
     set_selected_repository(repo);
   };
@@ -38,15 +37,24 @@ const App: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       {isLoggedIn ? (
-          !selected_repository ? ( 
-            repositories.length === 0 ? (
-              <div>Loading repositories...</div>
-            ) : (
-              <RepositoriesList user_repositories={repositories} onRepositoryClick={handleRepositoryClick} />
-            )
+        !selected_repository ? (
+          repositories.length === 0 ? (
+            <div>Loading repositories...</div>
           ) : (
-            <Board github={{token: github_user_info.token, user: github_user_info.user, repo: selected_repository}}/>
+            <RepositoriesList
+              user_repositories={repositories}
+              onRepositoryClick={handleRepositoryClick}
+            />
           )
+        ) : (
+          <Board
+            github={{
+              token: github_user_info.token,
+              user: github_user_info.user,
+              repo: selected_repository,
+            }}
+          />
+        )
       ) : (
         <div className="text-center">
           <h1 className="text-2xl font-bold mb-4">TrelloHub</h1>
@@ -56,6 +64,6 @@ const App: React.FC = () => {
       )}
     </div>
   );
-}
+};
 
 export default App;
